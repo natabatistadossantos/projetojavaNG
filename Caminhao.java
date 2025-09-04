86398ef327b36ddefeb41b9c9a22b2fea3f68dbe
GUILHERME GARCIA DOS SANTOS <guilherme.santos05@cs.up.edu.br>
2025-09-03 21:01:24 -0300

    teste gui

--- Caminhao.java
+++ Caminhao.java
@@ -1,3 +1,1 @@
-public class Caminhao {
-        private double capacidadeCarga;
-}
+
---
+++ Carro.java
@@ -0,0 +1,16 @@
+public class Carro {
+private int numeroPortas;
+
+    public int getNumeroPortas() {
+        return numeroPortas;
+    }
+
+    public void setNumeroPortas(int numeroPortas) {
+        this.numeroPortas = numeroPortas;
+    }
+
+
+
+
+
+}
