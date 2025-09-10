fc4975fc673bd7bd0fb5d1b4a7a98614366f7a58
natabatistadossantos <nata_santos18@hotmail.com>
2025-09-09 22:26:49 -0300

    mexendo no aluguel

--- Aluguel.java
+++ Aluguel.java
@@ -1,3 +1,31 @@
-public class Aluguel {
+public class Aluguel { private int id;
+    private Cliente cliente;
+    private Veiculo veiculo;
+    private int diasAlugados;
+    private double valortotal;
+
+    //construtor 
+    public Aluguel(int id, Cliente cliente, Veiculo veiculo, int diasAlugados, double valortotal)
+        this.id = id;
+        this.cliente = cliente;
+        this.veiculo = veiculo;
+        this.diasAlugados = diasAlugados;
+        this.valortotal = calcularValorTotal();
+    }
+
+    //getters e setters
+    public int getId(){return id;}
+    public void setId(int id){this.id=id;}
+    public Cliente getCliente (){return cliente;}
+    public void SetCliente(Cliente cliente){this.cliente=cliente;}
+    public Veiculo getVeiculo(){return veiculo;}
+    public void setVeiculo(Veiculo veiculo){this.veiculo=veiculo;}
+    public int getDiasAlugados(){return diasAlugados;}
+    public void setDiasAlugados(int diasAlugados){this.diasAlugados=diasAlugados;}
+
+    public double calcularValorTotal(){
+        return diasAlugados* Veiculo.getDiasAlugados;
+    }
+
     
 }
